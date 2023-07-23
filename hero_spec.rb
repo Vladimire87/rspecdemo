108d5fc2a5497d5712bc23d5066440663a92fe76
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-07-23 17:23:30 +0400

    add test for power up

--- hero_spec.rb
+++ hero_spec.rb
@@ -4,4 +4,8 @@ describe Hero do
     hero = Hero.new "jojo"
     expect(hero.name).to eq "Jojo"
   end
+  it "can power up" do
+    hero = Hero.new "jojo"
+    expect(hero.power_up).to eq 110
+  end
 end
