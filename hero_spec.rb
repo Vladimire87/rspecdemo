0cf5ad5614a3b0fa100a95d661d59f11496577f6
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-07-24 14:14:05 +0400

    add test for hero_info

--- hero_spec.rb
+++ hero_spec.rb
@@ -12,4 +12,9 @@ describe Hero do
     hero = Hero.new "jojo"
     expect(hero.power_down).to eq 90
   end
+
+  it "displays full hero info" do
+    hero = Hero.new "jojo"
+    expect(hero.hero_info).to eq "Jojo has a health of 100"
+  end
 end
